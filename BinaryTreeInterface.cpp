9148e6ff2e17e8c44564c537cc5bc61f76915750
kajm1ller <kajm@bgsu.edu>
2025-04-08 13:32:47 -0400

    add cpp definitions

--- BinaryTreeInterface.cpp
+++ BinaryTreeInterface.cpp
@@ -1,1 +1,2 @@
 #include "BinaryTreeInterface.h"
+
--- LinkedBTree.cpp
+++ LinkedBTree.cpp
@@ -1,1 +1,9 @@
 #include "LinkedBTree.h"
+#include "BinaryTreeInterface.h"
+#include "BinaryNode.h"
+
+template <typename T>
+int LinkedBTree::getHeightHelper(BinaryNode<T>) const {
+
+}
+
